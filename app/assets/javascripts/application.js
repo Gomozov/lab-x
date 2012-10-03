6722fa1731765ef6ed2619c08fea5bc235f75af7
Gomozov <Valeriy.gomozov@gmail.com>
2012-10-03 23:29:19 +0400

    Second

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,61 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree 
+  function CGauge(canvas, color, max, label, symbol) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.W = this.canvas.width;
+    this.H = this.canvas.height;
+    this.degrees = 0;
+    this.new_degrees = 0;
+    this.difference = 0;
+    this.color = color;                         
+    this.bgcolor = "#FFF";
+    this.text = "";
+    this.animation_loop = 0;
+    this.maxvalue = max;
+    this.minvalue = 0;
+    this.label = label;
+    this.symbol = symbol;
+  
+this.init = function () {
+      this.ctx.clearRect(0, 0,this.W, this.H);
+      this.ctx.beginPath();
+      this.ctx.strokeStyle = "#e1e1e1";// this.bgcolor;
+      this.ctx.lineWidth = 12;
+      this.ctx.arc(this.W/2, this.H/2, 38, 0, Math.PI*2, false); 
+      this.ctx.stroke();
+      radians = this.degrees * Math.PI / 180;
+      this.ctx.beginPath();
+      this.ctx.strokeStyle = this.color;
+      this.ctx.lineWidth = 12;
+      this.ctx.arc(this.W/2, this.H/2, 38, 0 - 90*Math.PI/180, radians - 90*Math.PI/180, false); 
+      this.ctx.stroke();
+      this.ctx.fillStyle = this.color;
+      this.ctx.font = "28px bebas";
+      this.text = Math.round(this.degrees/360*this.maxvalue)+this.symbol;
+      text_width = this.ctx.measureText(this.text).width;
+      this.ctx.fillText(this.text, this.W/2 - text_width/2, this.H/2 + 10);
+      this.ctx.font = "12px bebas";
+      label_width = this.ctx.measureText(this.label).width;
+      this.ctx.fillText(this.label,this.W - label_width - 10, this.H);
+}
+
+this.draw = function (temp) {
+  if(this.animation_loop != 0) clearInterval(this.animation_loop);
+  if(temp < this.minvalue) this.new_degrees = 0;
+  if(temp > this.maxvalue) this.new_degrees = 360;
+  if(temp < this.maxvalue && temp > this.minvalue) this.new_degrees = Math.round(360*temp/(this.maxvalue - this.minvalue));
+  this.difference = this.new_degrees - this.degrees;
+  this.animation_loop = setInterval((function(self){return function() {self.animate();}})(this), 1000/this.difference);
+}
+
+this.animate = function () {
+    if(this.degrees == this.new_degrees) clearInterval(this.animation_loop);
+    if(this.degrees < this.new_degrees)
+      this.degrees++;
+    else
+      this.degrees--;
+    this.init();
+}
+}
